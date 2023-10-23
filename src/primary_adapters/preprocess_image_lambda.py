85c0fcccd5070a340e62bf629114bad1e9478b96
Josh Clark <jclarktennis@gmail.com>
2023-10-23 08:01:25 +0000

    feat: add Lambda for 'preprocessing' images

---
+++ src/primary_adapters/preprocess_image_lambda.py
@@ -0,0 +1,29 @@
+"""Download the image from S3, read its date, and upload it to S3."""
+import json
+import os
+
+import boto3
+
+from src.core.prepare_images import get_image_date
+from src.secondary_adapters.image_format_readers import WhatImageIFR
+
+
+def lambda_handler(event, context):
+    s3 = boto3.resource("s3")
+    input_bucket = s3.Bucket(os.environ["S3_TO_BE_PREPARED_BUCKET"])
+    output_bucket = s3.Bucket(os.environ["S3_TO_BE_APPENDED_BUCKET"])
+
+    # Download image from S3
+    input_bucket.download_file(
+        event["Records"][0]["s3"]["object"]["key"], "/tmp/image.jpg"
+    )
+
+    # Read date
+    image_date = get_image_date("/tmp/image.jpg", WhatImageIFR).strftime(
+        "%Y_%m_%d_%H_%M_%S"
+    )
+
+    # Upload to S3 with date as key
+    output_bucket.upload_file("/tmp/image.jpg", f"{image_date}.jpg")
+
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
